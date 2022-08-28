b379bc87bc3af8f568cc98159c115bf085fb698b
panda <919401990@qq.com>
2022-08-28 16:30:14 +0800

    ✨ feat:添加声明文件

---
+++ dist/index.d.ts
@@ -0,0 +1,34 @@
+interface IOptions {
+    timer: number;
+    mode: "loop" | "once";
+    callback: () => any;
+    debug: boolean;
+}
+declare type modeType = "loop" | "once";
+declare class PausableTimer {
+    private id;
+    private timer;
+    private startTime;
+    private diffTime;
+    private isDubgger;
+    private callback;
+    private mode;
+    private opentionMeat;
+    constructor(option: IOptions);
+    start: () => void;
+    resume: () => void;
+    pause: () => void;
+    stop: () => void;
+    getNowTime: () => number;
+    setMode: (mode: modeType, isReset?: false) => void;
+    reset: () => void;
+    getState: () => {
+        diffTime: number;
+        startTime: number;
+        timer: number;
+        mode: modeType;
+    };
+    checkMode: (mode: modeType) => void;
+}
+
+export { PausableTimer as default };
